7b321ed0479f586a5d0133741e4592ccf7636c0b
Ninja.Win <lurongkai@live.com>
2013-02-21 22:35:29 +0800

    downgrade .net framework version from v4.5 to v4.0

--- src/Shinetech.TianJin.VpnAddressTracer/Properties/AssemblyInfo.cs
+++ src/Shinetech.TianJin.VpnAddressTracer/Properties/AssemblyInfo.cs
@@ -24,7 +24,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
 [assembly: AssemblyKeyFile("Shinetech.LuRongkai.snk")]
 [assembly: InternalsVisibleTo("Shinetech.TianJin.VpnAddressTracer.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100fdc4b7f5701e087394cfc62a6149750e4080335f8999cd6c7dc77b250bafa35b0736ccead8840e7e61ceba4f8d787a781b3fa6ff9f14fcbf8c305bf5a32058d31555e214e8953b8b5f36a96b0751fae6a03a80707b0b8538fe9a553da0077ff888ebe7f6d3a5da4492108108db5780e842519c61f6fd1f85be0bf03c60bda5bd")]
