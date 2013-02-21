e567634778927f5a5714be583da4dcc5b6989343
Ninja.Win <lurongkai@live.com>
2013-02-22 00:59:57 +0800

    fix vs2012 testing bug while specficed assemble culture. release 1.2.0.0

--- src/Shinetech.TianJin.VpnAddressTracer.Tests/Properties/AssemblyInfo.cs
+++ src/Shinetech.TianJin.VpnAddressTracer.Tests/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Shinetech.TianJin.VpnAddressTracer")]
 [assembly: AssemblyCopyright("Copyright © Shinetech Software Inc. 2013 - Lu Rongkai")]
 [assembly: AssemblyTrademark(" Shinetech Software Inc.")]
-[assembly: AssemblyCulture("en-US")]
+[assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(true)]
 
@@ -26,6 +26,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
 [assembly: AssemblyKeyFile("Shinetech.LuRongkai.snk")]
